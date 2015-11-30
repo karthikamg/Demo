82c962ccc1e1d75823167f3897ace1a3b184cb0a
karthika <karthikam@qburst.com>
2015-11-30 17:13:35 +0530

    Added the links and sublinks with styles

--- js/script.js
+++ js/script.js
@@ -19,12 +19,54 @@ app.config(function($routeProvider) {
           .when('/framework', {
             templateUrl : '/templates/frameworks.html'
         })
-        .when('/stylingComparison', {
+        .when('/stylingContent', {
             templateUrl : '/templates/styling-content.html'
         })
         .when('/stylingComparison1', {
+            templateUrl : '/templates/sass-content.html'
+        })
+        .when('/stylingComparison2', {
             templateUrl : '/templates/scss-content.html'
+        })
+        .when('/stylingComparison3', {
+            templateUrl : '/templates/compass-content.html'
         });
+        // .when('/variables', {
+        //     templateUrl : '/templates/sassTemplates/variables.html'
+        // })
+        // .when('/nesting', {
+        //     templateUrl : '/templates/sassTemplates/nesting.html'
+        // })
+        // .when('/partials', {
+        //     templateUrl : '/templates/sassTemplates/partials.html'
+        // })
+        // .when('/import', {
+        //     templateUrl : '/templates/sassTemplates/import.html'
+        // })
+        // .when('/mixins', {
+        //     templateUrl : '/templates/sassTemplates/mixins.html'
+        // })
+        // .when('/inheritance', {
+        //     templateUrl : '/templates/sassTemplates/inheritance.html'
+        // })
+        // .when('/back', {
+        //     templateUrl : '/templates/scss-content.html'
+        // })
+        // .when('/nestingback', {
+        //     templateUrl : '/templates/scss-content.html'
+        // })
+        // .when('/inheritback', {
+        //     templateUrl : '/templates/scss-content.html'
+        // })
+        // .when('/partialsback', {
+        //     templateUrl : '/templates/scss-content.html'
+        // })
+        // .when('/importback', {
+        //     templateUrl : '/templates/scss-content.html'
+        // })
+        //  .when('/mixinsback', {
+        //     templateUrl : '/templates/scss-content.html'
+        // });
 });
 
 
